a7a4bcdda892e4e5be3492533498c42d0774937c
mako4th <mako4th@snow.plala.or.jp>
2023-03-25 22:22:58 +0900

    プロジェクト ファイルを追加します。

---
+++ wpfLauncher/extras.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Interop;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace wpfLauncher
+{
+    class extras
+    {
+        public ImageSource getImageSourceFromUri(string uriString)
+        {
+            BitmapImage bitmapImage = new BitmapImage();
+            bitmapImage.BeginInit();
+            bitmapImage.UriSource = new Uri(uriString);
+            bitmapImage.EndInit();
+
+            return bitmapImage;
+        }
+        public ImageSource GetImageFromAssets(string filename)
+        {
+            string readPath = "Assets/" + filename;
+            BitmapImage bpim = new BitmapImage();
+            using (FileStream st = File.OpenRead(readPath))
+            {
+                bpim.BeginInit();
+                bpim.StreamSource = st;
+                bpim.DecodePixelWidth = 500;
+                bpim.CacheOption = BitmapCacheOption.OnLoad;
+                bpim.CreateOptions = BitmapCreateOptions.None;
+                bpim.EndInit();
+                bpim.Freeze();
+
+            }
+            return bpim;
+        }
+
+        [DllImport("gdi32.dll", EntryPoint = "DeleteObject")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool DeleteObject([In] IntPtr hObject);
+        public ImageSource getImageSourceFromResources(string filename)
+        {
+
+            IntPtr handle = Properties.Resources.partially_cloudy.GetHbitmap();
+            ImageSource result = null;
+
+            try
+            {
+                result = Imaging.CreateBitmapSourceFromHBitmap(handle, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+
+            }
+            finally
+            {
+                DeleteObject(handle);
+            }
+
+            return result;
+        }
+
+        private string getLinkTargetPath(string path)
+        {
+            string result = "";
+            dynamic? shell = null;
+            dynamic? lnk = null;
+
+            try
+            {
+                var type = Type.GetTypeFromProgID("WScript.Shell");
+                shell = Activator.CreateInstance(type);
+                lnk = shell.CreateShortcut(path);
+
+
+                if (string.IsNullOrEmpty(lnk.TargetPath))
+                {
+                    MessageBox.Show("リンク先が見つかりませんでした");
+                }
+
+                var linkPropaty = new
+                {
+                    lnk.Arguments,
+                    lnk.Description,
+                    lnk.FullName,
+                    lnk.Hotkey,
+                    lnk.IconLocation,
+                    lnk.TargetPath,
+                    lnk.WindowStyle,
+                    lnk.WorkingDirectory
+                };
+
+                result = lnk.TargetPath;
+            }
+            finally
+            {
+                if (lnk != null) Marshal.ReleaseComObject(lnk);
+                if (shell != null) Marshal.ReleaseComObject(shell);
+            }
+            return result;
+        }
+
+        public string getOriginPath(string path)
+        {
+
+            if (System.IO.Path.GetExtension(path) == ".lnk")
+            {
+                return getLinkTargetPath(path);
+            }
+            else
+            {
+                return path;
+            }
+
+        }
+
+        /*
+                    var icon = System.Drawing.Icon.ExtractAssociatedIcon(filepath);
+                    var bitmap = icon.ToBitmap();
+                    using (var ms = new System.IO.MemoryStream())
+                    {
+                        // MemoryStreamに書き出す
+                        bitmap.Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
+                        // MemoryStreamをシーク
+                        ms.Seek(0, System.IO.SeekOrigin.Begin);
+                        // MemoryStreamからBitmapFrameを作成
+                        // (BitmapFrameはBitmapSourceを継承しているのでそのまま渡せばOK)
+                        System.Windows.Media.Imaging.BitmapSource bitmapSource =
+                            System.Windows.Media.Imaging.BitmapFrame.Create(
+                                ms,
+                                System.Windows.Media.Imaging.BitmapCreateOptions.None,
+                                System.Windows.Media.Imaging.BitmapCacheOption.OnLoad
+                            );
+                    }
+
+        */
+    }
+}
---
+++ wpfLauncher/mainGrid.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+//using static Microsoft.WindowsAPICodePack.Shell.PropertySystem.SystemProperties.System;
+
+namespace wpfLauncher
+{
+    class mainGrid : Grid
+    {
+        MediaElement mediaElement;
+
+        public mainGrid()
+        {
+            mediaElement = new MediaElement();
+
+            //グリッド分割数の設定
+            int rowsCount = 10;
+            int columnsCount = 10;
+
+            //下から何行をメッセージ、広告エリアに設定するか
+            int webRows = 2;
+
+            int webColumns = columnsCount / 2;
+            int messColumns = columnsCount - webRows;
+
+
+            //グリッド作成
+            for (int i = 0; i < columnsCount; i++)
+            {
+                this.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+
+            for (int i = 0; i < rowsCount; i++)
+            {
+                this.RowDefinitions.Add(new RowDefinition());
+            }
+
+            //グリッドにimgを追加する
+            extras ex = new extras();
+            for (int r = 0; r < rowsCount - webRows; r++)
+            {
+                for (int l = 0; l < columnsCount; l++)
+                {
+                    System.Windows.Controls.Image img = new System.Windows.Controls.Image();
+                    //img.Source = ex.GetImageFromAssets("partially-cloudy.png");
+                    img.Source = ex.getImageSourceFromUri("https://illustimage.com/photo/dl/4976.png");
+
+                    img.MouseDown += imageMouseDown;
+                    img.Name = "img" + r.ToString() + l.ToString();
+                    img.Margin = new Thickness(1);
+                    Grid.SetRow(img, r);
+                    Grid.SetColumn(img, l);
+                    this.Children.Add(img);
+                }
+            }
+            this.Background = Brushes.Navy;
+            //下段左側
+            Grid.SetRow(mediaElement, 8);
+            Grid.SetColumn(mediaElement, 0);
+            Grid.SetRowSpan(mediaElement, 2);
+            Grid.SetColumnSpan(mediaElement, 5);
+            this.Children.Add(mediaElement);
+            mediaElement.LoadedBehavior = MediaState.Manual;
+            mediaElement.Visibility = Visibility.Visible;
+            mediaElement.Source = new Uri("Assets/nanohana909201.wmv", UriKind.Relative);
+            mediaElement.ClipToBounds = true;
+            mediaElement.StretchDirection = StretchDirection.Both;
+            mediaElement.Stretch = Stretch.UniformToFill;
+            mediaElement.MediaEnded += Media_Ended;
+            mediaElement.Play();
+
+            //下段右側
+            
+            WebBrowser web = new WebBrowser();
+            web.ClipToBounds = true;
+            var axIWebBrowser2 = typeof(WebBrowser).GetProperty("AxIWebBrowser2", BindingFlags.Instance | BindingFlags.NonPublic);
+            var comObj = axIWebBrowser2.GetValue(web, null);
+            comObj.GetType().InvokeMember("Silent", BindingFlags.SetProperty, null, comObj, new object[] { true });
+            Grid.SetRow(web, 8);
+            Grid.SetColumn(web, 5);
+            Grid.SetRowSpan(web, 2);
+            Grid.SetColumnSpan(web, 5);
+            this.Children.Add(web);
+            Uri uri = new Uri(@"http://www.microsoft.com");
+            web.Navigate(uri);
+        }
+
+        private void Media_Ended(object sender, EventArgs e)
+        {
+            mediaElement.Position = TimeSpan.Zero;
+            mediaElement.Play();
+        }
+
+        private void imageMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is System.Windows.Controls.Image)
+            {
+                System.Windows.Controls.Image im = (System.Windows.Controls.Image)sender;
+                MessageBox.Show(im.Name);
+            }
+        }
+    }
+}
